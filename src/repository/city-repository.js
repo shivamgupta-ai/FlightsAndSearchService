31deb7967482ea731e5144048d6b85f66c6f18d6
shivamgupta-ai <shivam917982@gmail.com>
2026-03-25 17:03:34 +0530

    Completed city repository

--- src/repository/city-repository.js
+++ src/repository/city-repository.js
@@ -1,12 +1,16 @@
 const { City } = require('../models/index');
 
 class CityRepository {
+    
     async createCity({ name }) {
         try {
-            const city = await City.create({ name });
+            const city = await City.create({
+                name
+            });
             return city;
         } catch (error) {
-            throw error;
+            console.log("Something went wrong in the repository layer");
+            throw { error };
         }
     }
 
@@ -17,10 +21,38 @@ class CityRepository {
                     id: cityId
                 }
             });
+            return true;
         } catch (error) {
-            throw error;
+            console.log("Something went wrong in the repository layer");
+            throw { error };
         }
     }
+
+    async updateCity(cityId, data) {
+        try {
+            const city = await City.update(data, {
+                where: {
+                    id: cityId
+                }
+            });
+            return city;
+        } catch (error) {
+            console.log("Something went wrong in the repository layer");
+            throw { error };
+        }
+    }
+
+    async getCity(cityId) {
+        try {
+            const city = await City.findByPk(cityId);
+            return city;
+        } catch (error) {
+            console.log("Something went wrong in the repository layer");
+            throw { error };
+        }
+    }
+
+
 }
 
 
