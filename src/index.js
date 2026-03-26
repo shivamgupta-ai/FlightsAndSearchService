fda287eb8579527c3a44f296600bf6992fb6589e
shivamgupta-ai <shivam917982@gmail.com>
2026-03-27 02:51:05 +0530

    Added routers to complete create api

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 
+
 const { PORT } = require("./config/serverConfig");
+const ApiRoutes = require("./routes/index");
 
 const setupAndStartServer = async () => {
     //create the express object
@@ -9,6 +11,8 @@ const setupAndStartServer = async () => {
 
     app.use(bodyParser.json());
     app.use(bodyParser.urlencoded({ extended: true}));
+
+    app.use("/api", ApiRoutes);
 
 
     app.listen(PORT, () => {
